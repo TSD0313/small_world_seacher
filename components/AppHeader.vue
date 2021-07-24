a82ae61d9dcbbaeda00253f188f6a83e984ee0be
TSD0313 <toride0313@gmail.com>
2021-07-24 17:06:19 +0900

    reload

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,7 @@
     <nav class="shadow p-3 mb-5 bg-primary rounded">
       <div class="container">
         <div class="navbar-brand">
-          <a class="navbar-item text-white font-weight-bold">Small World Searcher</a>
+          <a onclick="window.location.reload();" class="navbar-item text-white font-weight-bold">Small World Searcher</a>
         </div>
       </div>
     </nav>
